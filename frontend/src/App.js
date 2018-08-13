0221c33658c0720b6ef27cff7934ba431a0e2b79
Bono Stebler <bono@base7booking.com>
2018-08-13 16:20:30 +0200

    [Client] Update dependencies, setup react hot reloading

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,14 @@
 import React, { Component } from 'react';
 import './App.css';
 
+import Dashboard from 'components/dashboard/dashboard';
+
 class App extends Component {
     render() {
         return (
-            <div className="app"></div>
+            <div className="app">
+                <Dashboard />
+            </div>
         );
     }
 }
