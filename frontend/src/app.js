23ac78c1d4f8160fcde53212c0bf3599d11a9f76
Bono Stebler <bono@base7booking.com>
2018-08-14 11:43:43 +0200

    [Client] Lowercase app root files

--- frontend/src/app.js
+++ frontend/src/app.js
@@ -2,8 +2,19 @@ import React, { Component } from 'react';
 import { Provider } from 'react-redux';
 import { BrowserRouter, Route } from 'react-router-dom';
 
+import Dashboard from 'containers/dashboard/dashboard';
+import History from 'containers/history/history';
 import SignIn from 'containers/sign-in/sign-in';
 import SignUp from 'containers/sign-up/sign-up';
+import RequireLogin from 'containers/require-login';
+
+import './app.css';
+
+const requireLogin = (WrappedComponent) => () => (
+    <RequireLogin>
+        <WrappedComponent />
+    </RequireLogin>
+);
 
 class App extends Component {
     render() {
@@ -13,6 +24,8 @@ class App extends Component {
             <Provider store={store}>
                 <BrowserRouter>
                     <React.Fragment>
+                        <Route exact path="/" render={requireLogin(Dashboard)} />
+                        <Route path="/history" render={requireLogin(History)} />
                         <Route path="/sign-in" component={SignIn} />
                         <Route path="/sign-up" component={SignUp} />
                     </React.Fragment>
