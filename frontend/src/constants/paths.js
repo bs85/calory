896d2a53f3f403708468b8435d475e24c4a2abfc
Bono Stebler <bono@base7booking.com>
2018-08-14 09:23:52 +0200

    [Client] Add paths

---
+++ frontend/src/constants/paths.js
@@ -0,0 +1,5 @@
+export const PATH_ROOT = '/';
+export const PATH_SIGN_UP = '/sign-up';
+export const PATH_SIGN_IN = '/sign-in';
+export const PATH_DASHBOARD = PATH_ROOT;
+export const PATH_HISTORY = '/history';
