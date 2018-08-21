815081757713030bec9ebe01db1e5f3a3bf8870d
Bono Stebler <bono@base7booking.com>
2018-08-21 16:32:37 +0200

    [Client] Style components/notice

--- client/src/components/widgets/notice.js
+++ client/src/components/widgets/notice.js
@@ -1,17 +1,25 @@
 import Paper from '@material-ui/core/Paper';
+import { withStyles } from '@material-ui/core/styles';
 
 import React, { Component } from 'react';
 
+const styles = (theme) => ({
+    root: {
+        marginTop: theme.spacing.unit * 3,
+        padding: theme.spacing.unit * 3,
+    },
+});
+
 class Notice extends Component {
     render() {
-        const { children, type } = this.props;
+        const { classes, children, type } = this.props;
 
         return (
-            <Paper className={`${type}`}>
+            <Paper className={`${type} ${classes.root}`}>
                 { children }
             </Paper>
         );
     }
 }
 
-export default Notice;
+export default withStyles(styles)(Notice);
