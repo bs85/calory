5d174a6431d8682ba0085fb3288d070bfc17c4df
Bono Stebler <bono@base7booking.com>
2018-08-21 02:23:42 +0200

    [Server] Add cookie authentication

---
+++ server/server.js
@@ -0,0 +1,48 @@
+const loopback = require('loopback');
+const boot = require('loopback-boot');
+const path = require('path');
+
+const app = loopback();
+
+app.start = () => (
+    // start the web server
+    app.listen(() => {
+        app.emit('started');
+        const baseUrl = app.get('url').replace(/\/$/, '');
+        console.log('Web server listening at: %s', baseUrl);
+        if (app.get('loopback-component-explorer')) {
+            const explorerPath = app.get('loopback-component-explorer').mountPath;
+            console.log('Browse your REST API at %s%s', baseUrl, explorerPath);
+        }
+    })
+);
+
+// Bootstrap the application, configure models, datasources and middleware.
+// Sub-apps like REST API are mounted via boot scripts.
+boot(
+    app,
+    {
+        appRootDir: path.join(__dirname, '../config'),
+        appConfigRootDir: path.join(__dirname, '../config'),
+        modelSources: [
+            'loopback/common/models',
+            'loopback/server/models',
+            path.join(__dirname, '../src/models'),
+        ],
+        mixinSources: [],
+        bootDirs: [path.join(__dirname, '../boot')],
+    },
+    (err) => {
+        if (err) throw err;
+
+        app.use(loopback.token({
+            cookies: ['access_token'],
+            model: app.models.UserAccessToken,
+        }));
+
+        // start the server if `$ node server.js`
+        if (require.main === module) app.start();
+    },
+);
+
+module.exports = app;
